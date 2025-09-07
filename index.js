4d4273f8251f223b63a266d919a9621848f34b0e
prienka17 <prienka17@gmail.com>
2025-09-07 20:34:50 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log(window);
+alert("Hello....... it's Prienka'sFolio");
+const date = new Date();
+const n = date.toDateString();
+const time = date.toLocaleTimeString();
+document.write('Date: ' + n+"<br>");
+document.write('Time: ' + time);
